a7f5b1246f4438ec5766c61da0adcc2417bfd817
SamuelForte <samuelforte97@gmail.com>
2020-04-22 20:39:12 -0300

    versao 1.1

--- caculadora.py
+++ caculadora.py
@@ -1,2 +1,37 @@
 #calculadora em python
+operacao = input('''
 
+Digite a operação matemática que você deseja concluir:
++ para adicao
+- para subtracao
+* para multiplicacao
+/ para divisiao
+''')
+
+
+numero1 = int(input("digite seu primeiro numero: ")) #entrada de dados
+numero2 = int(input("digite seu segundo numerp: ")) #entrada de dados
+
+#adição
+if operacao == '+':
+print('{} + {} = '.format(numero1, numero2))  #formatadores de string
+print (numero1 + numero2) #mostra a soma dos numeros
+
+#subtração
+elif operacao == '-':
+print('{} - {} = '.format(numero1, numero2))  #formatadores de string
+print (numero1 - numero2) #mostra a subtração dos numeros
+
+#multiplicação
+elif operacao == '*':
+print('{} * {} = '.format(numero1, numero2))  #formatadores de string
+print (numero1 * numero2)#mostra a multiplicação
+
+#mostra a divisão
+elif operacao == '/':
+print('{} / {} = '.format(numero1, numero2))  #formatadores de string
+print (numero1 / numero2)
+
+else:
+    print ('Você não digitou um operador válido, execute o programa novamente.
+')
